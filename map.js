cedadd1cc43c56de6b68ffb17efd6dabc8e7c373
Atheer M <43179208+atheer10@users.noreply.github.com>
2019-10-20 16:32:44 +0300

    Create map.js

---
+++ map.js
@@ -0,0 +1,25 @@
+// MAP
+​
+// Write a function capitalize that takes a string and uses .map to return the same string in all caps.
+// ex. capitalize('whoop') // => 'WHOOP'
+// ex. capitalize('hello stranger') // => "HELLO STRANGER"
+var capitalize = function(string) {
+  // code code code!
+};
+​
+// Now write a new function called swapCase that takes a string of words and uses .map and your newly written capitalize()
+// function to return a string where every other word is in all caps.
+// Hint: look up Array.prototype.map on MDN and see what arguments the .map callback can take.
+// ex: swapCase('hello stranger , what do we have today? ') // => " HELLO stranger , WHAT do WE have TODAY ?"
+var swapCase = function(string) {
+  // Codeeeee
+};
+​
+// Write a function shiftLetters that takes a string and uses .map to return an encoded string with each letter shifted down the
+// alphabet by one. Hint: Use Look up the JS functions String.fromCharCode() and String.charCodeAt() .
+// see if you can use Ascii code to acomplish this
+// ex. shiftLetters('hello') // => 'ifmmp'
+// ex. (shiftLetters('abcxyz') // => "bcdyz{"
+var shiftLetters = function(string) {
+  // code!
+};
