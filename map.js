65b0a5dd14d6565d367d5527cc449e281e2b7508
Atheer M <43179208+atheer10@users.noreply.github.com>
2019-10-20 16:50:29 +0300

    Update map.js

--- map.js
+++ map.js
@@ -1,13 +1,10 @@
-// MAP
+​// ------ FOR ALL THE EXCERCISES ONLY USE ARROW FUNCTIONS  ----- //
 ​
-// Write a function capitalize that takes a string and uses .map to return the same string in all caps.
-// ex. capitalize('whoop') // => 'WHOOP'
-// ex. capitalize('hello stranger') // => "HELLO STRANGER"
-var capitalize = function(string) {
-  // code code code!
-};
-​
-// Now write a new function called swapCase that takes a string of words and uses .map and your newly written capitalize()
+
+
+// PART 1:  USE MAP IN THE FOLLOWING EXCERCISES //
+
+// A) write a function called swapCase that takes a string of words and uses .map and your newly written capitalize()
 // function to return a string where every other word is in all caps.
 // Hint: look up Array.prototype.map on MDN and see what arguments the .map callback can take.
 // ex: swapCase('hello stranger , what do we have today? ') // => " HELLO stranger , WHAT do WE have TODAY ?"
@@ -15,7 +12,7 @@ var swapCase = function(string) {
   // Codeeeee
 };
 ​
-// Write a function shiftLetters that takes a string and uses .map to return an encoded string with each letter shifted down the
+// B) Write a function shiftLetters that takes a string and uses .map to return an encoded string with each letter shifted down the
 // alphabet by one. Hint: Use Look up the JS functions String.fromCharCode() and String.charCodeAt() .
 // see if you can use Ascii code to acomplish this
 // ex. shiftLetters('hello') // => 'ifmmp'
@@ -23,3 +20,40 @@ var swapCase = function(string) {
 var shiftLetters = function(string) {
   // code!
 };
+
+
+
+
+// PART 2: USE FOREACH IN THE FOLLOWING EXCERCISES
+
+// A) Build a Deck
+// Use a forEach() loop within another forEach() loop to build an array representing a deck of cards. A deck consists of 52 cards - 13 ranks in each of 4 suits.
+// Then, display a list of every card in the deck. (Hint: Each element of the array should read something like "ace of spades" or "4 of hearts").
+// The start of your output should look something like this:
+// - ace of clubs
+// - 2 of clubs 
+// - 3 of clubs 
+// - 4 of clubs 
+// - 5 of clubs 
+// - 6 of clubs 
+// - 7 of clubs 
+// - 8 of clubs 
+// - 9 of clubs 
+// - 10 of clubs 
+// - jack of clubs 
+// - queen of clubs 
+// - king of clubs 
+// - ace of diamonds
+// - 2 of diamonds
+// - 3 of diamonds 
+
+
+// B) Word Play
+// Create a form where users may enter a sentence.
+// Turn that sentence into an array using the split method.
+// Then, loop through this array to build a new array out of every word in the sentence that is 3 or more characters in length.
+// Finally, reverse the order of the new array, join it back together into a string, and display it to the user.
+
+
+
+
