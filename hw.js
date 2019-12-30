233c2bc5f39634cd8707ef68190df11c5c391c40
wardahmad <whitelight43@hotmail.com>
2019-12-30 14:02:06 +0300

    almost

--- hw.js
+++ hw.js
@@ -77,3 +77,12 @@ console.log(resultarr)
 // Turn that sentence into an array using the split method.
 // Then, loop through this array to build a new array out of every word in the sentence that is 3 or more characters in length.
 // Finally, reverse the order of the new array, join it back together into a string, and display it to the user.
+
+var str = "hi my name is ward"
+var spl = str.split(" ");
+//console.log(spl)
+var arr = [];
+
+var check = spl.filter(element => element.length >= 3);
+console.log(check)
+var result = check.forEach(word => console.log(`${word} `))
